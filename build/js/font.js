36bfb681f6c72ebd2e0eed3abd2b294bd1ebbfde
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-07-18 17:14:14 -0300

    update tokens json

--- build/js/font.js
+++ build/js/font.js
@@ -1,8 +1,16 @@
 /**
  * Do not edit directly
- * Generated on Fri, 15 Jul 2022 20:07:11 GMT
+ * Generated on Mon, 18 Jul 2022 20:13:25 GMT
  */
 
 export const GlobalFontWeightBold = "Bold";
 export const GlobalFontWeightMedium = "Medium";
 export const GlobalFontWeightRegular = "Regular";
+export const GlobalFontSizeBase = "1rem";
+export const GlobalFontSizeXxxs = "1rem * 0.75";
+export const GlobalFontSizeXxs = "1rem * 0.875";
+export const GlobalFontSizeSm = "1rem * 1.125";
+export const GlobalFontSizeMd = "1rem * 2";
+export const GlobalFontSizeLg = "1rem*1.75";
+export const GlobalFontSizeXl = "1rem*2.25";
+export const GlobalFontSizeXxl = "1rem * 2.5";
--- build/js/spacing.js
+++ build/js/spacing.js
@@ -1,14 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Fri, 15 Jul 2022 20:07:11 GMT
+ * Generated on Mon, 18 Jul 2022 20:13:25 GMT
  */
 
-export const GlobalSpacingNano = "0.5rem / 2 ";
 export const GlobalSpacingBase = "0.5rem";
+export const GlobalSpacingNano = "0.5rem * 0.25 ";
 export const GlobalSpacingXs = "0.5rem * 1.5";
 export const GlobalSpacingSm = "0.5rem * 2";
 export const GlobalSpacingMd = "0.5rem * 3";
 export const GlobalSpacingLg = "0.5rem * 4";
 export const GlobalSpacingXl = "0.5rem * 5";
 export const GlobalSpacingXxl = "0.5rem * 6";
-export const GlobalSpacingXxxl = "0.5rem * 7";
+export const GlobalSpacingXxx = "0.5rem * 7";
+export const GlobalSpacingGiant = "0.5rem * 8";
