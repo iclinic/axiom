4fe9c1a4776d304454f0564b0089fd9555f971c4
Ivan de Freitas Jr <freitasjr.ivan@gmail.com>
2022-10-27 11:37:37 -0300

    feat: manually generate distribution files

---
+++ base/index.js
@@ -0,0 +1,19 @@
+const border = require('../dist/js/border')
+const color = require('../dist/js/color')
+const font = require('../dist/js/font')
+const line = require('../dist/js/line')
+const opacity = require('../dist/js/opacity')
+const shadow = require('../dist/js/shadow')
+const spacing = require('../dist/js/spacing')
+
+const base = {
+  border,
+  color,
+  font,
+  line,
+  opacity,
+  shadow,
+  spacing,
+}
+
+module.exports = base
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+console.warn(`
+In order to use this package, please check the available themes at https://github.com/iclinic/axiom#readme\n
+You can also import from base tokens using "@axiom/token/base"
+`)
