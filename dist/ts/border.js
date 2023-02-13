14e08338248568f20a210b23a4c96877e2df8481
Gabriel Reichardt Gueiros <gabrielrgueiros@live.com>
2023-02-13 11:59:32 -0300

    fix: import path on iclinic tokens

---
+++ dist/ts/border.js
@@ -0,0 +1,23 @@
+"use strict";
+/**
+ * Do not edit directly
+ * Generated on Mon, 13 Feb 2023 14:54:00 GMT
+ */
+exports.__esModule = true;
+exports.border = void 0;
+exports.border = {
+    "radius": {
+        "none": "0px",
+        "sm": "4px",
+        "md": "8px",
+        "lg": "16px",
+        "xl": "24px",
+        "xxl": "32px",
+        "circular": "100%"
+    },
+    "width": {
+        "none": "0px",
+        "xs": "1px",
+        "sm": "2px"
+    }
+};
---
+++ dist/ts/font.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * Do not edit directly
+ * Generated on Mon, 13 Feb 2023 14:54:00 GMT
+ */
+exports.__esModule = true;
+exports.font = void 0;
+exports.font = {
+    "lineHeight": {
+        "xxs": "100%",
+        "xs": "116%",
+        "sm": "128%",
+        "md": "132%",
+        "lg": "142%",
+        "xl": "150%"
+    },
+    "weight": {
+        "regular": 400,
+        "medium": 500,
+        "bold": 700
+    },
+    "size": {
+        "xxxs": "0.75rem",
+        "xxs": "0.875rem",
+        "xs": "1rem",
+        "sm": "1.125rem",
+        "md": "1.5rem",
+        "lg": "1.75rem",
+        "xl": "2.25rem",
+        "xxl": "2.5rem"
+    },
+    "family": {
+        "base": "Roboto"
+    },
+    "letterSpacing": {
+        "small": "1%",
+        "medium": "4%"
+    }
+};
