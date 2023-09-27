c50bc465bf85b26a75af8ef77b5f818fcbd6c16b
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2023-09-27 10:56:22 -0300

    feat: change buttons tokens

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -1,27 +1,63 @@
 import * as React from 'react'
 
 import { Slot } from '@radix-ui/react-slot'
-import { cva, type VariantProps } from 'class-variance-authority'
+import {
+  cva,
+  type VariantProps as CvaVariantProps,
+} from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  [
+    'focus-visible:ring-ring',
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'rounded-sm',
+    'text-sm',
+    'filter-none',
+    'focus-visible:outline-none',
+    'focus-visible:ring-2',
+    'font-medium',
+    'transition-colors',
+    'focus-visible:ring-offset-2',
+    'disabled:pointer-events-none',
+    'disabled:opacity-50',
+  ],
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default: [
+          'bg-interactive-accent-default',
+          'text-onInteractive-accent',
+          'hover:bg-interactive-accent-hovered',
+        ],
+        destructive: [
+          'bg-interactive-alert-accent-default',
+          'text-onInteractive-alert-accent',
+          'hover:bg-interactive-alert-accent-hovered',
+        ],
+        outline: [
+          'border-interactive-accent-default',
+          'hover:border-interactive-accent-hovered',
+          'text-interactive-accent-default',
+          'hover:text-interactive-accent-hovered',
+          'border',
+        ],
+        secondary: [
+          'bg-interactive-subtle-default',
+          'text-onInteractive-subtle',
+          'hover:bg-interactive-subtle-hovered',
+        ],
+        ghost: [
+          'hover:bg-interactive-subtle-hovered',
+          'text-onInteractive-subtle',
+        ],
+        link: ['text-link-default', 'underline-offset-4', 'hover:underline'],
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-4 py-3',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
@@ -34,9 +70,14 @@ const buttonVariants = cva(
   },
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+interface NativeProps extends React.ComponentPropsWithoutRef<'button'> {
+  //
+}
+interface VariantProps extends CvaVariantProps<typeof buttonVariants> {
+  //
+}
+
+export interface ButtonProps extends VariantProps, NativeProps {
   asChild?: boolean
 }
 
@@ -52,6 +93,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   },
 )
+
 Button.displayName = 'Button'
 
 export { Button, buttonVariants }
