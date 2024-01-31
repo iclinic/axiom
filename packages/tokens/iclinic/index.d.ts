b1bc5033e54cfb1c9b043f18e382e77e8a834919
Gabriel Reichardt Gueiros <gabriel.gueiros@zarpsystem.com.br>
2024-01-31 10:04:30 -0300

    chore: adjust shadow token values
    [PLT-168]

--- packages/tokens/iclinic/index.d.ts
+++ packages/tokens/iclinic/index.d.ts
@@ -524,11 +524,11 @@ export declare const color: {
         };
     };
 }, shadowBrand: {
-    readonly "1": "0px 1px 2px 0px rgba(4, 110, 236, 0.06)";
+    readonly "1": "0px 1px 2px 0px rgba(4, 110, 236, 0.06), 0px 3px 4px -4px rgba(4, 110, 236, 0.06)";
     readonly "2": "0px 1px 2px 0px rgba(4, 110, 236, 0.06), 0px 5px 4px -4px rgba(4, 110, 236, 0.06)";
-    readonly "3": "0px 2px 2px 0px rgba(4, 110, 236, 0.06), 0px 0px 2px 0px rgba(4, 110, 236, 0.06)";
+    readonly "3": "0px 3px 8px 0px rgba(4, 110, 236, 0.06), 0px 6px 6px 0px rgba(4, 110, 236, 0.06)";
     readonly "4": "0px 12px 12px -8px rgba(4, 110, 236, 0.15), 0px 0px 4px 0px rgba(4, 110, 236, 0.06)";
-    readonly "5": "0px 32px 40px -16px rgba(4, 110, 236, 0.15), 0px 2px 6px 0px rgba(4, 110, 236, 0.06)";
+    readonly "5": "0px 32px 40px -16px rgba(4, 110, 236, 0.15), 0px 2px 6px -4px rgba(4, 110, 236, 0.06)";
     readonly "6": "0px 24px 48px -8px rgba(4, 110, 236, 0.15), 0px 48px 64px -24px rgba(4, 110, 236, 0.15)";
 }, border: {
     readonly none: "0px";
@@ -566,12 +566,12 @@ export declare const color: {
     readonly xxl: "32px";
     readonly full: "10000px";
 }, shadow: {
-    readonly "1": "0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 5px 4px -4px rgba(0, 0, 0, 0.04)";
-    readonly "2": "0px 2px 2px 0px rgba(0, 0, 0, 0.1), 0px 0px 2px 0px rgba(0, 0, 0, 0.04)";
-    readonly "3": "0px 12px 12px -8px rgba(0, 0, 0, 0.17), 0px 0px 4px 0px rgba(0, 0, 0, 0.04)";
-    readonly "4": "0px 32px 40px -16px rgba(0, 0, 0, 0.17), 0px 2px 6px 0px rgba(0, 0, 0, 0.1)";
-    readonly "5": "0px 24px 48px -12px rgba(0, 0, 0, 0.23), 0px 48px 64px -24px rgba(0, 0, 0, 0.1)";
-    readonly "6": "0px 24px 48px -8px rgba(0, 0, 0, 0.17), 0px 48px 64px -24px rgba(0, 0, 0, 0.17)";
+    readonly "1": "0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 3px 4px -4px rgba(0, 0, 0, 0.04)";
+    readonly "2": "0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 5px 4px -4px rgba(0, 0, 0, 0.04)";
+    readonly "3": "0px 3px 8px 0px rgba(0, 0, 0, 0.04), 0px 6px 6px 0px rgba(0, 0, 0, 0.04)";
+    readonly "4": "0px 12px 12px -8px rgba(0, 0, 0, 0.1), 0px 0px 4px 0px rgba(0, 0, 0, 0.04)";
+    readonly "5": "0px 32px 40px -16px rgba(0, 0, 0, 0.1), 0px 2px 6px -4px rgba(0, 0, 0, 0.04)";
+    readonly "6": "0px 24px 48px -8px rgba(0, 0, 0, 0.1), 0px 48px 64px -24px rgba(0, 0, 0, 0.1)";
 }, dimension: {
     readonly "0": "0rem";
     readonly "1": "0.25rem";
